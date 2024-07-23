33dc93b22bc3db3fdef9eb848cbc70e0b11ffcb3
Mayson <mayson.reader.00@education.tas.edu.au>
2024-07-23 22:29:55 +1000

    1.0.0

---
+++ src/main/java/com/sirnugget/createmail/CreateMailClient.java
@@ -0,0 +1,10 @@
+package com.sirnugget.createmail;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateMailClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/sirnugget/createmail/CreateMailDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sirnugget.createmail;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateMailDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
